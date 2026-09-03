c316c54d1a10726e14cbf999de0fde8bf42572ea
Manuel Porcincula <manuellucioporcinculaxworld@gmail.com>
2026-09-03 23:20:26 +0800

    Added Sidebar

--- src/script.js
+++ src/script.js
@@ -1,3 +1,12 @@
 const date = document.querySelector("#date");
-
+const asidebar = document.querySelector(".asidebar");
+const burgerbtn = document.querySelector(".burgerbtn");
+const closebtn = document.querySelector(".closebtn");
 date.textContent = new Date().getFullYear();
+burgerbtn.addEventListener("click", () => {
+  asidebar.classList.remove("translate-x-full");
+});
+
+closebtn.addEventListener("click", () => {
+  asidebar.classList.add("translate-x-full");
+});
